81af9377470e23ff5b59e9a420b6754d099811bd
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2018-05-24 15:04:21 +0200

    setup.py downloads CDK, builds CDKBridge.class

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 import os
 from setuptools import setup, find_packages
 from subprocess import check_output
+import subprocess
+import py4j
+import os
+
+py4j_path = os.path.join(*py4j.__path__[0].split('/')[:-4])
+py4j_jar_path = os.path.join('/', py4j_path, 'share', 'py4j', 'py4j' + py4j.__version__ + '.jar')
+
+cdk_version = 'cdk-2.1.1'
+cdk_jar_path = './cdk_pywrapper/cdk/'
+cdk_jar = cdk_jar_path + cdk_version + '.jar'
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
@@ -8,9 +18,16 @@ MICRO_VERSION = 1
 
 REPO_URL = 'https://github.com/sebotic/cdk_pywrapper'
 
+subprocess.check_call(["wget https://github.com/cdk/cdk/releases/download/{0}/{0}.jar "
+                 "--directory-prefix={1}".format(cdk_version, cdk_jar_path)], shell=True)
+
+subprocess.check_call(["javac -cp '{}:{}' ./cdk_pywrapper/cdk/cdk_bridge.java".format(py4j_jar_path,
+                                                                                      cdk_jar)], shell=True)
+
 setup(
     name='cdk_pywrapper',
     version="{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION),
+    data_files=[("share/cdk", [cdk_jar, './cdk_pywrapper/cdk/CDKBridge.class'])],
     author='Sebastian Burgstaller-Muehlbacher',
     author_email='sburgs@scripps.edu',
     description='Python wrapper for the CDK (Chemistry Development Kit)',
@@ -19,7 +36,7 @@ setup(
     url=REPO_URL,
     # packages=find_packages(),
     packages=['cdk_pywrapper'],
-    include_package_data=True,
+    # include_package_data=True,
     # long_description=read('README.md'),
     classifiers=[
         "Programming Language :: Python",
