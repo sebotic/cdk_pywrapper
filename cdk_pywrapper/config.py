8a067d2c81fd6dafac2ae77eaecdbe248e16d0d7
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2018-05-24 15:55:26 +0200

    changed py4j version

--- cdk_pywrapper/config.py
+++ cdk_pywrapper/config.py
@@ -1,3 +1,4 @@
 # get the the py4j server jar file with 'find /usr/ -type f -name  py4j*jar'
-py4j_path = '/usr/local/share/py4j/py4j0.10.6.jar'
+# better: use pip: pip3 show -f py4j
+py4j_path = '/usr/local/share/py4j/py4j0.10.7.jar'
 cdk_path = './cdk/cdk-1.5.13.jar'
